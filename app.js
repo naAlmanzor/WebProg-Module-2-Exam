923c4c5ef12dc2c9840bc9d1a1b4524c9461dafd
naAlmanzor <neilalmanzor28@gmail.com>
2022-01-06 19:04:13 +0800

    Added variables

---
+++ app.js
@@ -0,0 +1,42 @@
+var images = [
+    "https://randomuser.me/api/portraits/lego/1.jpg",
+    "https://randomuser.me/api/portraits/lego/2.jpg",
+    "https://randomuser.me/api/portraits/lego/3.jpg",
+    "https://randomuser.me/api/portraits/lego/4.jpg",
+    "https://randomuser.me/api/portraits/lego/5.jpg",
+    "https://randomuser.me/api/portraits/lego/6.jpg",
+    "https://randomuser.me/api/portraits/lego/7.jpg",
+    "https://randomuser.me/api/portraits/lego/8.jpg",
+    "https://randomuser.me/api/portraits/lego/9.jpg",
+    "https://randomuser.me/api/portraits/lego/10.jpg",
+];
+
+var names = [
+    "Kubo",
+    "Nunu",
+    "Gobta",
+    "Beni Enma",
+    "Koyanskaya",
+    "Hina",
+    "Momoji",
+    "Cidala",
+    "Vikala",
+    "Junnie",
+];
+
+var age = [
+    '2', '5', '10', '8', '6', '11', '3', '4', '8', '1'
+];
+
+var breed = [
+    "Syberian Husky",
+    "Persian",
+    "Eurasian Otter",
+    "Parrot",
+    "Silver Fox",
+    "Hamster",
+    "Goldfish",
+    "Toyger",
+    "Deer Mouse",
+    "Shiba Inu",
+];
